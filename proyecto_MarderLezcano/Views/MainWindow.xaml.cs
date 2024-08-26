0d82af791791c923f56452aeb01d195b42de6f88
Melisa Lezcano Airaldi <melisalezcanoairaldi@gmail.com>
2024-08-26 11:25:00 -0300

    prueba 1

--- proyecto_MarderLezcano/Views/MainWindow.xaml.cs
+++ proyecto_MarderLezcano/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace proyecto_MarderLezcano
+namespace proyecto_MarderLezcano.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
