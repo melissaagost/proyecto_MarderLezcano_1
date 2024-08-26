3e534bcc04db8208e3c3c92149b2f2901d48e3de
Melisa Lezcano Airaldi <melisalezcanoairaldi@gmail.com>
2024-08-26 12:29:41 -0300

    prueba de vista en login

--- proyecto_MarderLezcano/ViewModels/loginViewModel.cs
+++ proyecto_MarderLezcano/ViewModels/loginViewModel.cs
@@ -1,12 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using proyecto_MarderLezcano.Commands;
+
 
 namespace proyecto_MarderLezcano.ViewModels
 {
-    class loginViewModel
+    class LoginViewModel : INotifyPropertyChanged
     {
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(Login);
+        }
+
+        private void Login()
+        {
+            // Aquí iría la lógica de autenticación, como verificar el usuario y contraseña
+            if (Username == "admin" && Password == "password")
+            {
+                // Autenticación exitosa
+                // Por ejemplo, abrir la ventana principal de la aplicación
+            }
+            else
+            {
+                // Mostrar mensaje de error
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
