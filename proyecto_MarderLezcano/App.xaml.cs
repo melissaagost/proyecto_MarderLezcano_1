1ba687ca71934ffcb0efab9ff741629719f43c7f
Melisa Lezcano Airaldi <melisalezcanoairaldi@gmail.com>
2024-09-24 17:01:16 -0300

    conectado :)))

--- proyecto_MarderLezcano/App.xaml.cs
+++ proyecto_MarderLezcano/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using proyecto_MarderLezcano.Database;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,23 @@ namespace proyecto_MarderLezcano
     /// </summary>
     public partial class App : Application
     {
+        string connectionString = ConfigurationManager.ConnectionStrings["medilink"].ConnectionString;
+        // Puedes usar esta cadena de conexión para conectar con la base de datos
+
+
+        //establecemos conexion y muestra mensaje si hay error
+            protected override void OnStartup(StartupEventArgs e)
+            {
+                base.OnStartup(e);
+                DatabaseManager dbManager = new DatabaseManager();
+                if (!dbManager.OpenConnection()) // Intenta abrir la conexión al inicio
+                {
+                    MessageBox.Show("No se pudo conectar a la base de datos. Por favor, verifica tu conexión o configuración.", "Error de Conexión", MessageBoxButton.OK, MessageBoxImage.Error);
+                    this.Shutdown(); // Cierra la aplicación si no puede conectar
+                }
+            }
+        
+
     }
 
 }
